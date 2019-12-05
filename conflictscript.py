4d165b498842e8ffd79f9fd81e1675cd0a89ac0d
aquacap <58543570+aquacap@users.noreply.github.com>
2019-12-05 14:06:32 +0800

    conflicting script

---
+++ conflictscript.py
@@ -0,0 +1,16 @@
+from urllib.request import urlopen
+import json
+
+def get_weather(city):
+    garbabge code that cannot         work
+
+
+
+
+
+if __name__ == "__main__":
+    degrees = get_weather("OSLO")
+    print("Weather in Oslo is %.2f degrees Celsius" % degrees)
+
+    location = postal_lookup("B323PP")
+    print(location)
